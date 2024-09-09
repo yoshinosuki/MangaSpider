b438aac96e1eff390457fa6572e865c74185fb47
yoshinosuki <tomotake20020625@gmail.com>
2024-09-09 13:52:07 +0800

    1.4

--- utils/download.py
+++ utils/download.py
@@ -2,6 +2,23 @@ import subprocess
 import os
 from concurrent.futures import ThreadPoolExecutor
 import sys
+import requests
+
+
+def test_web():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE",
+    }
+    max_retries = 4  # 最大重试次数
+    for i in range(max_retries):
+        try:
+            r = requests.get('https://nhentai.net', headers=headers, stream=True)
+            if r.status_code == 200:
+                print(f"网络测试成功")
+                return True
+        except requests.exceptions.RequestException as e:
+            print(f'Retry {i + 1}/{max_retries} after {e}')
+            pass
 
 
 def run_script(script):
@@ -15,13 +32,19 @@ def run_script(script):
             log_file.write(stderr.decode('utf-8'))
 
 
-print("开始下载进程")
-path = "../book"
-os.makedirs(path, exist_ok=True)
+def main():
+    print("开始下载进程")
+    path = "../book"
+    os.makedirs(path, exist_ok=True)
 
-scripts = ["./tools/get_book_1.py", "./tools/get_book_2.py", "./tools/get_book_3.py"]
+    scripts = ["./tools/get_book_1.py", "./tools/get_book_2.py", "./tools/get_book_3.py"]
 
-with ThreadPoolExecutor() as executor:
-    executor.map(run_script, scripts)
+    with ThreadPoolExecutor() as executor:
+        executor.map(run_script, scripts)
 
-print("下载完成，下载记录已保存")
+    print("下载完成，下载记录已保存")
+
+
+if __name__ == '__main__':
+    if test_web():
+        main()
